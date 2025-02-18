8ade91b47b2db74b06af0a20c5079317b7856206
ManuFlorenca! <manuelaflorenca22@gmail.com>
2025-02-18 11:13:55 -0300

    Password Generator (Está com erro)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import prompt from "prompt";
 import mainPrompt from "./prompts/prompt-main.js";
 import createQRCode from "./services/create.js";
+import createPassword from "./services/password/create.js";
 
 
 async function main(){
     prompt.get(mainPrompt,async(err, choose) => {
         if(choose.select == 1) await createQRCode();
-        if(choose.select == 2)console.log("Escolheu o Password");
+        if(choose.select == 2) await createPassword();
     });
     prompt.start();
 }
